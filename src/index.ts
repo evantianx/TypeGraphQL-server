fafc995f11e47c674b2053f9d954946691e44f6e
Rubicker <newsl00001@gmail.com>
2019-01-15 19:07:56 +0800

    :sparkles: Add forgetpassword & changepassword

--- src/index.ts
+++ src/index.ts
@@ -6,22 +6,13 @@ import { createConnection } from "typeorm";
 import session from "express-session";
 import connectRedis from "connect-redis";
 
-import { RegisterResolver } from "./modules/user/Register";
 import { redis } from "./redis";
 import cors from "cors";
-import { LoginResolver } from "./modules/user/Login";
-import { MeResolver } from "./modules/user/Me";
-import { ConfirmUserResolver } from "./modules/user/ConfirmUser";
 
 const main = async () => {
   await createConnection();
   const schema = await buildSchema({
-    resolvers: [
-      RegisterResolver,
-      LoginResolver,
-      MeResolver,
-      ConfirmUserResolver
-    ],
+    resolvers: [__dirname + "/modules/**/*.ts"],
     authChecker: ({ context: { req } }) => {
       return !!req.session.userId;
     }
--- src/modules/user/ConfirmUser.ts
+++ src/modules/user/ConfirmUser.ts
@@ -1,13 +1,14 @@
 import { Resolver, Mutation, Arg } from "type-graphql";
 import { User } from "../../entity/user";
 import { redis } from "../../redis";
+import { confirmUserPrefix } from "../constants/redisPrefixes";
 
 @Resolver()
 export class ConfirmUserResolver {
   @Mutation(() => Boolean)
   async confirmUser(@Arg("token") token: string): Promise<boolean> {
     // fake async in this example
-    const userId = await redis.get(token);
+    const userId = await redis.get(`${confirmUserPrefix}${token}`);
     if (!userId) {
       return false;
     }
