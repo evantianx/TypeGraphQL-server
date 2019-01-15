de7a7639d6e7bf01f79025cc06096f590bb297d0
Rubicker <newsl00001@gmail.com>
2019-01-15 17:10:51 +0800

    :sparkles: Add confirmation email

--- src/index.ts
+++ src/index.ts
@@ -11,11 +11,17 @@ import { redis } from "./redis";
 import cors from "cors";
 import { LoginResolver } from "./modules/user/Login";
 import { MeResolver } from "./modules/user/Me";
+import { ConfirmUserResolver } from "./modules/user/ConfirmUser";
 
 const main = async () => {
   await createConnection();
   const schema = await buildSchema({
-    resolvers: [RegisterResolver, LoginResolver, MeResolver],
+    resolvers: [
+      RegisterResolver,
+      LoginResolver,
+      MeResolver,
+      ConfirmUserResolver
+    ],
     authChecker: ({ context: { req } }) => {
       return !!req.session.userId;
     }
--- src/modules/user/Register.ts
+++ src/modules/user/Register.ts
@@ -4,6 +4,8 @@ import { User } from "../../entity/user";
 import { RegisterInput } from "./register/RegisterInput";
 import { isAuth } from "../middleware/isAuth";
 import { logger } from "../middleware/logger";
+import { sendEmail } from "../utils/sendEmail";
+import { createConfirmationUrl } from "../utils/createConfirmationUrl";
 
 @Resolver()
 export class RegisterResolver {
@@ -30,6 +32,8 @@ export class RegisterResolver {
       email,
       password: hashedPassword
     }).save();
+
+    await sendEmail(email, await createConfirmationUrl(user.id));
     return user;
   }
 }
